eadd67e4d1a9c5d9518e1022ec2b67f08800aa62
Dmitry Ulanov <ulanov.ktn@gmail.com>
2020-02-27 11:38:19 +0700

    fxml button click

--- src/main/java/BoardForm.java
+++ src/main/java/BoardForm.java
@@ -1,19 +1,23 @@
+import java.net.URL;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.Parent;
 
 public class BoardForm extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Label l = new Label("Hello, FX");
-        Scene scn = new Scene(new StackPane(l),640,480);
-        stage.setScene(scn);
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("board.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
     public static void main(String[] args) {
-        Application.launch();
+        launch();
     }
 }
